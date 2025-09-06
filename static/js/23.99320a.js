ad433f45e6c07182ff187368483d24c92b8a98a7
z168188 <lyuelinm54@gmail.com>
2025-09-06 12:49:23 +0800

    Update 23.99320a.js

--- static/js/23.99320a.js
+++ static/js/23.99320a.js
@@ -13,5 +13,5 @@
 })();
 
 function btnDown() {
-    window.open('https://jasgsdhnayutjzg.s3.us-east-005.backblazeb2.com/lyueir-quickq8.01.9.zip')
+    window.open('https://f005.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_z64b9c51816b9c17a9a8a0311_f107d5eb7bec670e2_d20250906_m044811_c005_v0501011_t0052_u01757134091142')
                 }
